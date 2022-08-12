49b2e07464718ef21874e6ab24b70c4b5e810a8e
AlinaStratij <astratijcuk@gmail.com>
2022-08-13 01:28:14 +0300

    task 8 (changes)

--- js/task-08.js
+++ js/task-08.js
@@ -1,34 +1,13 @@
-// const form = document.querySelector(`.login-form`);
-// console.log(form);
-// form.addEventListener(`submit`, onFormSubmit);
-// const infoRef = {};
-// function onFormSubmit(event) {
-//     event.preventDefault();
-//     const inputFirstName = form.elements[0].name;
-//   const inputSecondName = form.elements[1].name;
-//   const inputFirstValue = form.elements[0].value;
-//   const inputSecondValue = form.elements[1].value;
-//   if (inputFirstValue === "" || inputSecondValue === "") {
-//     alert("Все поля должны быть заполнены!");
-//   } else {
-//     infoRef[inputFirstName] = inputFirstValue;
-//     infoRef[inputSecondName] = inputSecondValue;
-//     console.log(infoRef);
-//     event.target.reset();
-//   }
-// }
-const formRef = document.querySelector(".login-form");
-
-formRef.addEventListener("submit", onFormSubmit);
-
+const form = document.querySelector(`.login-form`);
+console.log(form);
+form.addEventListener(`submit`, onFormSubmit);
 const infoRef = {};
-
 function onFormSubmit(event) {
-  event.preventDefault();
-  const inputFirstName = formRef.elements[0].name;
-  const inputSecondName = formRef.elements[1].name;
-  const inputFirstValue = formRef.elements[0].value;
-  const inputSecondValue = formRef.elements[1].value;
+    event.preventDefault();
+    const inputFirstName = form.elements[0].name;
+  const inputSecondName = form.elements[1].name;
+  const inputFirstValue = form.elements[0].value;
+  const inputSecondValue = form.elements[1].value;
   if (inputFirstValue === "" || inputSecondValue === "") {
     alert("Все поля должны быть заполнены!");
   } else {
@@ -38,3 +17,4 @@ function onFormSubmit(event) {
     event.target.reset();
   }
 }
+
